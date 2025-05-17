da6e2b134c9004eda809afce0389737b6efb6f37
pytis <battist.becker@web.de>
2025-05-18 00:33:52 +0200

    gpu opti

--- main.py
+++ main.py
@@ -37,7 +37,10 @@ np.random.seed(seed)
 
 import tensorflow as tf
 tf.random.set_seed(seed)
+print("Physical GPUs:", tf.config.list_physical_devices("GPU"))
 
+from tensorflow.keras import mixed_precision
+mixed_precision.set_global_policy('mixed_float16')
 
 # initialize environment
 from environment import Environment
@@ -69,3 +72,9 @@ match mode:
                                model_path=model_path)
         else:
             print("No model param to validate")
+
+import time
+start = time.time()
+for _ in range(100):
+    dqn_agent.train_iterate()
+print("Zeit für 100 Trainingsschritte:", time.time() - start, "Sekunden")
