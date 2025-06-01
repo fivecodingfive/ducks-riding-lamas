931cc461fbaae0a2bbc2d2f91b242dd9df0616b4
fivecodingfive <jonas05.coding@gmail.com>
2025-06-01 13:08:21 +0200

    Resolve merge error

--- config.py
+++ config.py
@@ -27,29 +27,3 @@ parser.add_argument('--algorithm', type=str, default='dqn', choices=['dqn', 'ppo
                     help="Type of algorithm to use")
 
 args = parser.parse_args()
-import argparse
-
-parser = argparse.ArgumentParser(description="Train Tabular Q-learning Agent on GridWorld")
-
-parser.add_argument('--variant', type=int, default=0, choices=[0, 1, 2],
-                    help="Environment variant: 0 (base), 1 (extension 1), 2 (extension 2)")
-
-parser.add_argument('--data_dir', type=str, default='./data',
-                    help="Path to the data directory (e.g., ./data)")
-
-parser.add_argument('--episodes', type=int, default=300,
-                    help="Number of training episodes")
-
-parser.add_argument('--seed', type=int, default=42,
-                    help="Random seed for reproducibility")
-
-parser.add_argument('--mode', type=str, default='training', choices=['training', 'validation', 'testing'],
-                    help="Run mode for environment")
-
-parser.add_argument('--modelpath', type=str,
-                    help="Path to model parameters")
-
-parser.add_argument('--network', type=str, default='mlp', choices=['mlp', 'cnn', 'combine'],
-                    help="Type of neural network to use")
-
-args = parser.parse_args()
