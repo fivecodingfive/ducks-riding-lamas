17e3800db4739df43fbbb13bef7c220029989292
pytis <battist.becker@web.de>
2025-07-04 10:08:52 +0200

    döner preise verringern

--- agent_ppo/config.py
+++ agent_ppo/config.py
@@ -3,18 +3,18 @@ ppo_config = {
     "state_size": 10,
     "action_size": 5,
 
-    "gamma": 0.99, # discount factor -> if high, future rewards are more important, and if low, immediate rewards are more important
-    "lam": 0.9,
+    "gamma": 0.999, # discount factor -> if high, future rewards are more important, and if low, immediate rewards are more important
+    "lam": 0.925,
     "entropy": 0.1,
-    "entropy_decay": 0.98,  # decays every 5th episode with given factor
+    "entropy_decay": 0.96,  # decays every 5th episode with given factor
     "entropy_min": 0.0,  # minimum entropy value
-    "clip_ratio": 0.2,
-    "train_policy_epochs": 40,
-    "train_value_function_epochs": 40,
-    "policy_learning_rate": 0.0009, # Smaller learning rate -> Smaller updates (risk of getting stuck if too small)
-    "value_learning_rate": 0.002,
+    "clip_ratio": 0.1,
+    "train_policy_epochs": 5,
+    "train_value_function_epochs": 5,
+    "policy_learning_rate": 0.0003, # Smaller learning rate -> Smaller updates (risk of getting stuck if too small)
+    "value_learning_rate": 0.0005,
     "max_time_steps": 200,
 
-    "n_episodes": 20
+    "n_episodes": 200
 }
 
