938b4078963ecb6b705795b2a7187b55d951bb4e
pytis <battist.becker@web.de>
2025-07-06 20:13:56 +0200

    test gamma

--- agent_ppo/config.py
+++ agent_ppo/config.py
@@ -3,15 +3,15 @@ ppo_config = {
     "state_size": 10,
     "action_size": 5,
 
-    "gamma": 0.99, # discount factor -> if high, future rewards are more important, and if low, immediate rewards are more important
-    "lam": 0.9,
+    "gamma": 0.95, # discount factor -> if high, future rewards are more important, and if low, immediate rewards are more important
+    "lam": 0.84,
     "entropy": 0.0,
     "entropy_decay": 0.95,  # decays every 5th episode with given factor
     "entropy_min": 0.0,  # minimum entropy value
-    "clip_ratio": 0.3,
-    "train_epochs": 35,  
-    "policy_learning_rate": 0.0003, # Smaller learning rate -> Smaller updates (risk of getting stuck if too small)
-    "value_learning_rate": 0.001,
+    "clip_ratio": 0.4,
+    "train_epochs": 37,  
+    "policy_learning_rate": 0.0007, # Smaller learning rate -> Smaller updates (risk of getting stuck if too small)
+    "value_learning_rate": 0.0014,
     "max_time_steps": 200,
     "rollout_steps":   2048,
 
