4ddf835f514c451fe5c943a68c4dbcd8cbcb31ab
pytis <battist.becker@web.de>
2025-07-08 10:53:08 +0200

    smaller vector

--- main_ppo.py
+++ main_ppo.py
@@ -121,7 +121,7 @@ if USE_WANDB:
             entity="ducks-riding-llamas",
             project="ride-those-llamas",
             name=run_name,
-            group= "job15",
+            group= "job17",
             config={
                 "env_variant": args.variant,
                 "ppo_config": ppo_config,
