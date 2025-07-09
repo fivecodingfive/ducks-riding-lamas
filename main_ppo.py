6357d2e7f2a197afb5a1c2e1219add92c9e17dbe
pytis <battist.becker@web.de>
2025-07-09 22:28:38 +0200

    how stupid can one be??

--- main_ppo.py
+++ main_ppo.py
@@ -121,7 +121,7 @@ if USE_WANDB:
             entity="ducks-riding-llamas",
             project="ride-those-llamas",
             name=run_name,
-            group= "new",
+            group= "fucking-idiot",
             config={
                 "env_variant": args.variant,
                 "ppo_config": ppo_config,
